07fb592b0e146af49c853f50db1a1cda6d0d905e
Hilario Gutierrez <hilario.m.gutierrez@gmail.com>
2022-11-11 15:46:51 -0300

    Se agregaron comentarios para ordenar el codigo

--- js/objetos.js
+++ js/objetos.js
@@ -15,6 +15,7 @@ console.log(destinosAumento);
 const equipajes = [
     {id:1, peso:23, precio:3399},
     {id:2, peso:12, precio:2599},
-    {id:3, peso:10, precio:2399}
+    {id:3, peso:10, precio:2399},
+    {id:4, peso:0, precio:0}
 ]
 
