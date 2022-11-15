8cdd6b7b55c8231666d74c6ffc0574f72abe7337
Hilario Gutierrez <hilario.m.gutierrez@gmail.com>
2022-11-15 15:55:35 -0300

    se agrego vinculo al index

---
+++ js/dom.js
@@ -0,0 +1,24 @@
+
+
+// Crear tarjetas de destinos
+// const pintarDestinos = () => {
+//     const card = document.getElementById("sectionCards")
+//     destinos.forEach(destino => {
+//         const div = document.createElement("div");
+//         div.classList.add("tarjetasDestino");
+//         div.innerHTML +=
+//             `<div class="card m-3" style="width: 18rem;">
+//             <a href="./pages/comprar.html"><img src="${destino.imagen}" class="card-img-top"
+//                 alt="parras de uva representando a la ciudad de mendoza">
+            
+//         </div>`
+
+//         card.append(div);
+
+//     })
+
+// }
+
+//pintarDestinos();
+
+
