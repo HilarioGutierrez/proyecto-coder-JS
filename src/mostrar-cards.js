3f3f7b04fabae8b0dcd6d74f12cb6aee8ad67f06
Hilario Gutierrez <hilario.m.gutierrez@gmail.com>
2022-12-11 14:21:19 -0300

    rutas img .json

--- src/mostrar-cards.js
+++ src/mostrar-cards.js
@@ -7,7 +7,7 @@ const pintarPedidoDestinos = async () => {
         const divDestinos = document.createElement("div");
         divDestinos.classList.add("d-flex", "justify-content-center", "justify-content-lg-around", "flex-row",);
         divDestinos.innerHTML = `
-    <a href="../pages/comprar.html" class="vinculo">
+    <a href="./pages/comprar.html" class="vinculo">
     <div class="d-flex flex-column align-items-center justify-content-center justify-content-lg-around cardDestino m-3">
         <h3 class="tituloTarjetasDestino text-center m-3">${destino.nombre}</h3>
         <img class="imagenDestino" src="${destino.imagen}" alt="">
